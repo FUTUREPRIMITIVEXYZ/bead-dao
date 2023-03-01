924b1ddfd9c3ef38699d607f0cd6a34d300a6dd3
Jayden Windle <jaydenwindle@gmail.com>
2023-03-01 14:12:00 -0700

    straight to bead gif

--- pages/api/metadata/beadz.tsx
+++ pages/api/metadata/beadz.tsx
@@ -5,7 +5,7 @@ const metadataHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     description: "ᗷEᗩᗪᘔ.EᑎᑕOᗰᑭᗩᔕᔕ.ᗩᒪᒪ.ᗪᖇEᗩᗰᔕ https://ilovebeadz.xyz",
     external_url: "https://ilovebeadz.xyz",
     image:
-      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYTI4MGEwZTMxMTQ3MGE5YTZmOGMzZjZiNDkzY2RiYjdjMTQ5NTM3MSZjdD1n/vHublFw0x9L44N36Ju/giphy.gif",
+      "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGQ2YWM1NWU4NGU5ZTk1ZWUxMDgzMDQyMmFkZDVkODZlMGIzY2JiYiZjdD1n/vHublFw0x9L44N36Ju/giphy.gif",
     name: `ᗷEᗩᗪ DAO BEADZ`,
   });
 };
