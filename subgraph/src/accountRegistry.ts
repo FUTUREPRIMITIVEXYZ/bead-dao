d48f2fa4a58d02dd48d5c623efe7f96a6b98f5e5
Jayden Windle <jaydenwindle@gmail.com>
2023-03-04 18:22:21 -0700

    added subgraph to index tokenbound accounts

---
+++ subgraph/src/accountRegistry.ts
@@ -0,0 +1,4 @@
+import { AccountCreated as AccountCreatedEvent } from "../generated/AccountRegistry/AccountRegistry";
+import { Lizard } from "../generated/schema";
+
+export function handleAccountCreated(event: AccountCreatedEvent): void {}
